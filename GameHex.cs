00d9a74307780d0dad2eb630fa7b480473d8e34b
jeffercizer <jeffercize@gmail.com>
2025-03-17 15:01:07 -0400

    Update GameHex.cs

--- GameHex.cs
+++ GameHex.cs
@@ -114,6 +114,8 @@ struct GameHexTests
     {
         GameHexTests.TestSpawnUnitSingle(false);
         GameHexTests.TestSpawnUnitMultipleStacking(false);
+        GameHexTests.TestSpawnUnitMultipleNoStackingFlexible(false);
+        GameHexTests.TestSpawnUnitMultipleNoStackingNoFlexible(false);
     }
     
     static public void TestSpawnUnitSingle(bool printGameBoard)
@@ -257,12 +259,128 @@ struct GameHexTests
         Unit testUnit = new Unit("testUnit", movementCosts, this);
         Unit testUnit2 = new Unit("testUnit2", movementCosts, this);
         Hex target = new Hex(1, 1, -2);
-        Hex flexTarget = new Hex(1, 1, -2);
+        Hex flexTarget = target.WrappingNeighbor(0);
         if (!mainBoard.gameHexDict[target].SpawnUnit(testUnit, false, true) | !mainBoard.gameHexDict[target].SpawnUnit(testUnit2, false, true))
         {
             Tests.Complain(name);
         }
         if (!mainBoard.gameHexDict[target].unitsList.Contains(testUnit) | !mainBoard.gameHexDict[flexTarget].unitsList.Contains(testUnit2))
+        {
+            Tests.Complain(name);
+        }
+        if (printGameBoard)
+        {
+            mainBoard.PrintGameBoard();
+        }
+    }
+    
+    static public void TestSpawnUnitSevenNoStackingFlexible(bool printGameBoard)
+    {
+        String name = "TestSpawnUnitSevenNoStackingFlexible";
+        int top = 0;
+        int bottom = 10;
+        int left = 0;
+        int right = 30;
+        Dictionary<Hex, GameHex> gameHexDict = new();
+        for (int r = top; r <= bottom; r++){
+            int r_offset = r>>1; //same as (int)Math.Floor(r/2.0f)
+            for (int q = left - r_offset; q <= right - r_offset; q++){
+                if(r==0 || r == bottom || q == left - r_offset || q == right - r_offset || (r == bottom/2 && q > left - r_offset + 2 && q < right - r_offset - 2))
+                {
+                    gameHexDict.Add(new Hex(q, r, -q-r), new GameHex(new Hex(q, r, -q-r), TerrainType.Flat, TerrainTemperature.Grassland, new HashSet<FeatureType>()));
+                }
+                else
+                {
+                    gameHexDict.Add(new Hex(q, r, -q-r), new GameHex(new Hex(q, r, -q-r), TerrainType.Flat, TerrainTemperature.Grassland, new HashSet<FeatureType>()));
+                }
+            }
+        }
+        GameBoard mainBoard = new GameBoard(gameHexDict, top, bottom, left, right);
+
+        Dictionary<TerrainMoveType, float> movementCosts = {
+            { TerrainMoveType.Flat, 1 },
+            { TerrainMoveType.Rough, 2 },
+            { TerrainMoveType.Mountain, 9999 },
+            { TerrainMoveType.Coast, 1 },
+            { TerrainMoveType.Ocean, 1 },
+            { TerrainMoveType.Forest, 1 },
+            { TerrainMoveType.River, 0 },
+            { TerrainMoveType.Road, 0.5f },
+            { TerrainMoveType.Embark, 0 },
+            { TerrainMoveType.Disembark, 0 },
+        };
+        Unit testUnit = new Unit("testUnit", movementCosts, this);
+        Unit testUnit2 = new Unit("testUnit2", movementCosts, this);
+        Unit testUnit3 = new Unit("testUnit3", movementCosts, this);
+        Unit testUnit4 = new Unit("testUnit4", movementCosts, this);
+        Unit testUnit5 = new Unit("testUnit5", movementCosts, this);
+        Unit testUnit6 = new Unit("testUnit6", movementCosts, this);
+        Unit testUnit7 = new Unit("testUnit7", movementCosts, this);
+        Hex target = new Hex(1, 1, -2);
+        Hex flexTarget2 = target.WrappingNeighbor(0);
+        Hex flexTarget3 = target.WrappingNeighbor(1);
+        Hex flexTarget4 = target.WrappingNeighbor(2);
+        Hex flexTarget5 = target.WrappingNeighbor(3);
+        Hex flexTarget6 = target.WrappingNeighbor(4);
+        Hex flexTarget7 = target.WrappingNeighbor(5);
+        if (!mainBoard.gameHexDict[target].SpawnUnit(testUnit, false, true) | !mainBoard.gameHexDict[flexTarget2].SpawnUnit(testUnit2, false, true) | !mainBoard.gameHexDict[flexTarget3].SpawnUnit(testUnit3, false, true) | !mainBoard.gameHexDict[flexTarget4].SpawnUnit(testUnit4, false, true) |!mainBoard.gameHexDict[flexTarget5].SpawnUnit(testUnit5, false, true) | !mainBoard.gameHexDict[flexTarget6].SpawnUnit(testUnit6, false, true) | !mainBoard.gameHexDict[flexTarget7].SpawnUnit(testUnit7, false, true))
+        {
+            Tests.Complain(name);
+        }
+        if (!mainBoard.gameHexDict[target].unitsList.Contains(testUnit) | !mainBoard.gameHexDict[flexTarget2].unitsList.Contains(testUnit2) | !mainBoard.gameHexDict[flexTarget3].unitsList.Contains(testUnit3) | !mainBoard.gameHexDict[flexTarget4].unitsList.Contains(testUnit4) | !mainBoard.gameHexDict[testUnit5].unitsList.Contains(testUnit5) | !mainBoard.gameHexDict[flexTarget6].unitsList.Contains(testUnit6) | !mainBoard.gameHexDict[flexTarget7].unitsList.Contains(testUnit7))
+        {
+            Tests.Complain(name);
+        }
+        if (printGameBoard)
+        {
+            mainBoard.PrintGameBoard();
+        }
+    }
+
+    static public void TestSpawnUnitMultipleNoStackingNoFlexible(bool printGameBoard)
+    {
+        String name = "TestSpawnUnitMultipleNoStackingNoFlexible";
+        int top = 0;
+        int bottom = 10;
+        int left = 0;
+        int right = 30;
+        Dictionary<Hex, GameHex> gameHexDict = new();
+        for (int r = top; r <= bottom; r++){
+            int r_offset = r>>1; //same as (int)Math.Floor(r/2.0f)
+            for (int q = left - r_offset; q <= right - r_offset; q++){
+                if(r==0 || r == bottom || q == left - r_offset || q == right - r_offset || (r == bottom/2 && q > left - r_offset + 2 && q < right - r_offset - 2))
+                {
+                    gameHexDict.Add(new Hex(q, r, -q-r), new GameHex(new Hex(q, r, -q-r), TerrainType.Flat, TerrainTemperature.Grassland, new HashSet<FeatureType>()));
+                }
+                else
+                {
+                    gameHexDict.Add(new Hex(q, r, -q-r), new GameHex(new Hex(q, r, -q-r), TerrainType.Flat, TerrainTemperature.Grassland, new HashSet<FeatureType>()));
+                }
+            }
+        }
+        GameBoard mainBoard = new GameBoard(gameHexDict, top, bottom, left, right);
+
+        Dictionary<TerrainMoveType, float> movementCosts = {
+            { TerrainMoveType.Flat, 1 },
+            { TerrainMoveType.Rough, 2 },
+            { TerrainMoveType.Mountain, 9999 },
+            { TerrainMoveType.Coast, 1 },
+            { TerrainMoveType.Ocean, 1 },
+            { TerrainMoveType.Forest, 1 },
+            { TerrainMoveType.River, 0 },
+            { TerrainMoveType.Road, 0.5f },
+            { TerrainMoveType.Embark, 0 },
+            { TerrainMoveType.Disembark, 0 },
+        };
+        Unit testUnit = new Unit("testUnit", movementCosts, this);
+        Unit testUnit2 = new Unit("testUnit2", movementCosts, this);
+        Hex target = new Hex(1, 1, -2);
+        Hex flexTarget = target.WrappingNeighbor(0);
+        if (!mainBoard.gameHexDict[target].SpawnUnit(testUnit, false, false) | mainBoard.gameHexDict[target].SpawnUnit(testUnit2, false, false))
+        {
+            Tests.Complain(name);
+        }
+        if (!mainBoard.gameHexDict[target].unitsList.Contains(testUnit) | mainBoard.gameHexDict[target].unitsList.Contains(testUnit2) | mainBoard.gameHexDict[flexTarget].unitsList.Contains(testUnit2))
         {
             Tests.Complain(name);
         }
