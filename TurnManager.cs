63bd575557ec29245659257a07532e58c84a174b
jeffercizer <jeffercize@gmail.com>
2025-03-18 08:25:32 -0400

    Update TurnManager.cs

--- TurnManager.cs
+++ TurnManager.cs
@@ -9,18 +9,14 @@ struct TurnManager
     public TurnManager(GameBoard mainBoard)
     {
       this.mainBoard = mainBoard;
-      OnNewTurn += mainBoard.OnTurnStarted;
     }
     GameBoard mainBoard;
-    public delegate void NewTurnEvent(int turnNumber);
-    public event NewTurnEvent OnNewTurn;
 
     private int currentTurn = 0;
 
     public void StartNextTurn()
     {
         currentTurn++;
-        Console.WriteLine($"Turn {currentTurn} started");
-        OnNewTurn?.Invoke(currentTurn);
+        mainBoard.OnTurnStarted();
     }
 }
