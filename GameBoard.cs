86cb9aefcb1f46e92cebb2939efe1f05bd1035f4
jeffercizer <jeffercize@gmail.com>
2025-03-19 14:22:51 -0400

    Update GameBoard.cs

--- GameBoard.cs
+++ GameBoard.cs
@@ -4,14 +4,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Data;
 
-public class GameTile
-{
-    public GameHex gameHex { get; set; } // Represents the tile's core data
-    public bool hasBeenSeen { get; set; } // Whether the tile has been revealed at least once by the active players team
-    public int unitsSeeing { get; set; } // Number of units currently seeing the tile on the active players team
-    
-}
-
 struct GameBoard
 {
     public GameBoard(int top, int bottom, int left, int right)
