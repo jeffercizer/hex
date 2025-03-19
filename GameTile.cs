bb5198a7f598e9faf8c6eb2fefb5361399dcda30
jeffercizer <jeffercize@gmail.com>
2025-03-19 14:35:58 -0400

    Update GameTile.cs

--- GameTile.cs
+++ GameTile.cs
@@ -6,11 +6,43 @@ using System.Data;
 
 struct GameTile
 {
-  public class GameTile
-  {
-      public GameHex gameHex { get; set; } // Represents the tile's core data
-      public bool hasBeenSeen { get; set; } // Whether the tile has been revealed at least once by the active players team
-      public int unitsSeeing { get; set; } // Number of units currently seeing the tile on the active players team
-  }
+    public class GameTile
+    {
+        public GameTile(GameHex gameHex)
+        {
+            gameHex = gameHex;
+            hasBeenSeen = false;
+            unitsSeeing = 0;
+        }
+        GameHex gameHex;
+        bool hasBeenSeen;
+        int unitsSeeing;
+        
+        public void RevealTile()
+        {
+            hasBeenSeen = true;
+        }
+        
+        public bool AddUnitSeeing()
+        {
+            unitsSeeing++;
+            if (unitsSeeing > 0)
+            {
+                RevealTile();
+                return true;
+            }
+            return false;
+        }
+        
+        public bool RemoveUnitSeeing()
+        {
+            if (unitsSeeing > 0)
+            {
+                unitsSeeing--;
+                return true;
+            }
+            return false;
+        }
+    }
 }
 
