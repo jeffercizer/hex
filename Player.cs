ced91ebf2862c30b9306917f2d14d8040f8b8a8d
jeffercizer <jeffercize@gmail.com>
2025-03-20 08:37:56 -0400

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -11,6 +11,15 @@ struct Player
         this.game = game;
         this.teamNum = teamNum;
     }
+    public Player(Game game, int teamNum, Dictionary<Hex, int> visibleGameHexDict, Dictionary<Hex, bool> seenGameHexDict)
+    {
+        this.game = game;
+        this.teamNum = teamNum;
+        this.visibleGameHexDict = visibleGameHexDict;
+        this.seenGameHexDict = seenGameHexDict;
+    }
     public Game game;
     public int teamNum;
+    public Dictionary<Hex, int> visibleGameHexDict;
+    public Dictionary<Hex, bool> seenGameHexDict;
 }
