58bad185cbac9fb0af32fb598d1c27444d307f16
jeffercizer <jeffercize@gmail.com>
2025-03-21 08:34:30 -0400

    Create District.cs

---
+++ District.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Data;
+
+[Serializable]
+public class District
+{
+    public District(GameHex ourGameHex, Building initialBuilding, bool isCityCenter, City ourCity)
+    {
+        buildings = new();
+        buildings.Add(initialBuilding);
+        this.ourGameHex = ourGameHex;
+        this.isCityCenter = isCityCenter;
+        this.ourCity = ourCity;
+        
+    }
+    public List<Building> buildings;
+    public GameHex ourGameHex;
+    public bool isCityCenter;
+    public City ourCity;
+}
