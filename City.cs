b07ebe96b9604cc6b33ecd4ccd5b0ef6d6d1fca0
jeffercizer <jeffercize@gmail.com>
2025-03-21 08:21:39 -0400

    Update City.cs

--- City.cs
+++ City.cs
@@ -4,18 +4,23 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Data;
 
-struct City
+class City
 {
-    public City(int id, int teamNum, String name)
+    public City(int id, int teamNum, String name, GameHex ourGameHex)
     {
         this.id = id;
         this.teamNum = teamNum;
         this.name = name;
+        this.ourGameHex = ourGameHex;
+        ourGameHex.ourGameBoard.game.playerDictionary[teamNum].AddCity(this);
+        districts = new();
+        districts.Add(new District(ourGameHex, new Building("City Center"), true, this));
     }
     public int id;
     public int teamNum;
     public String name;
     public List<District> districts;
+    public GameHex ourGameHex;
 
     public bool ChangeTeam(int newTeamNum)
     {
@@ -26,5 +31,10 @@ struct City
     public bool AddDistrict(District newDistrict)
     {
         districts.Add(newDistrict);
+    }
+
+    public String ChangeName(String name)
+    {
+        this.name = name;
     }
 }
