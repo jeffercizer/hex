fc8fabd776a6726e63186f89aa3ea3fc5f2f5c2d
jeffercizer <jeffercize@gmail.com>
2025-03-21 12:49:25 -0400

    Update City.cs

--- City.cs
+++ City.cs
@@ -49,6 +49,7 @@ public class City
         BuildingEffect effect = new BuildingEffect(effectFunction);
         building.AddEffect(effect);
         District district = new District(ourGameHex, building, true, this);
+        building.district = district;
         districts.Add(district);
     }
 
@@ -56,11 +57,6 @@ public class City
     {
         teamNum = newTeamNum;
         return true;
-    }
-
-    public bool AddDistrict(District newDistrict)
-    {
-        districts.Add(newDistrict);
     }
 
     public String ChangeName(String name)
@@ -84,11 +80,119 @@ public class City
         }
     }
 
-    public bool ExpandToHex(Hex hex)
+    public void BuildOnHex(Hex hex, Building building)
     {
+        if(!)
+        {
+            District district = new District(ourGameHex, building, false, this);
+            building.district = district;
+            districts.Add(district);
+        }
+        else
+        {
+            district.AddBuilding(building);
+            building.district = district;
+        }
+
+    }
+
+    public void ExpandToHex(Hex hex)
+    {
+        Building ruralBuilding = GetRuralBuilding(hex);
+        District district = new District(ourGameHex, ruralBuilding, false, this);
+        ruralBuilding.district = district;
+        districts.Add(district);
+    }
+
+    public Building GetRuralBuilding(Hex hex)
+    {
+        Building ruralBuilding;
+        switch (ourGameHex.ourGameBoard.gameHexDict[hex].terrainType)
+        {
+            case TerrainType.Flat:
+                ruralBuilding = new Building("Farm");
+                break;
+            case TerrainType.Rough:
+                ruralBuilding = new Building("Mine");
+                break;
+            case TerrainType.Mountain:
+                ruralBuilding = new Building("Hunting Camp");
+                break;
+            case TerrainType.Coast:
+                ruralBuilding = new Building("Fishing Boat");
+                break;
+            case TerrainType.Ocean:
+                ruralBuilding = new Building("Whaling Ship");
+                break;
+        }
+        return ruralBuilding;
+    }
+
+    public List<Hex> ValidExpandHexes(List<TerrainType> validTerrain)
+    {
+        List<Hex> validHexes = new();
+        //gather valid targets
         foreach(Hex hex in hex.Range(3))
         {
-            
+            if(validTerrainTypes.Contains(ourGameHex.ourGameBoard.gameHexDict[hex]))
+            {
+                //hex is unowned or owned by us so continue
+                if(ourGameHex.ourGameBoard.gameHexDict[hex].ownedBy == -1 | ourGameHex.ourGameBoard.gameHexDict[hex].ownedBy == teamNum)
+                {
+                    //hex does not have a district
+                    if (ourGameHex.ourGameBoard.gameHexDict[hex].district == null)
+                    {
+                        //hex has only allies unit
+                        bool valid = true;
+                        foreach(Unit unit in ourGameHex.ourGameBoard.gameHexDict[hex].unitsList)
+                        {
+                            if(!teamManager.GetAllies(teamNum).Contains(unit.teamNum))
+                            {
+                                valid = false;
+                            }
+                        }
+                        if(valid)
+                        {
+                            validHexes.Add(hex);
+                        }
+                    }
+                }
+            }
         }
+        return validHexes;
+    }
+
+    public List<Hex> ValidUrbanBuildHexes(List<TerrainType> validTerrain)
+    {
+        List<Hex> validHexes = new();
+        //gather valid targets
+        foreach(Hex hex in hex.Range(3))
+        {
+            if(validTerrainTypes.Contains(ourGameHex.ourGameBoard.gameHexDict[hex]))
+            {
+                //hex is unowned or owned by us so continue
+                if(ourGameHex.ourGameBoard.gameHexDict[hex].ownedBy == -1 | ourGameHex.ourGameBoard.gameHexDict[hex].ownedBy == teamNum)
+                {
+                    //hex does not have a district or it is not urban
+                    if (ourGameHex.ourGameBoard.gameHexDict[hex].district == null | !ourGameHex.ourGameBoard.gameHexDict[hex].district.isUrban)
+                    {
+                        //hex doesnt have a non-friendly unit
+                        bool valid = true;
+                        foreach(Unit unit in ourGameHex.ourGameBoard.gameHexDict[hex].unitsList)
+                        {
+                            if(!teamManager.GetAllies(teamNum).Contains(unit.teamNum))
+                            {
+                                valid = false;
+                            }
+                        }
+                        if(valid)
+                        {
+                            validHexes.Add(hex);
+                        }
+                    }
+                }
+            }
+        }
+        return validHexes;
     }
 }
