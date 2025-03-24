58c43e20cd6680063dff53d7182d209b8fecbdba
jeffercizer <jeffercize@gmail.com>
2025-03-24 10:13:41 -0400

    Update ResourceList.cs

--- ResourceList.cs
+++ ResourceList.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
 public enum ResourceType
 {
     None,
@@ -23,4 +28,43 @@ public enum ResourceType
     Coffee = 'e',
     Cotton = 'j',
     Tobacco = 't',
+}
+
+public struct ResourceInfo
+{
+    public int Food { get; set; }
+    public int Production { get; set; }
+    public int Gold { get; set; }
+}
+
+public class ResourceList()
+{
+    Dictionary<ResourceType, ResourceInfo> resources;
+    public ResourceList()
+    {
+        string xmlPath = "Resources.xml";
+        resources = LoadResourceData(xmlPath);
+        //example usage
+        //TileResource resource = TileResource.Wheat;
+        //if (resources.TryGetValue(resource, out ResourceInfo info))
+    }
+    public static Dictionary<TileResource, ResourceInfo> LoadResourceData(string xmlPath)
+    {
+        // Load the XML file
+        XDocument xmlDoc = XDocument.Load(xmlPath);
+
+        // Parse the resource information into a dictionary
+        var resourceData = xmlDoc.Descendants("Resource")
+            .ToDictionary(
+                r => (ResourceType)Enum.Parse(typeof(ResourceType), r.Attribute("Name").Value),
+                r => new ResourceInfo
+                {
+                    Food = int.Parse(r.Attribute("Food").Value),
+                    Production = int.Parse(r.Attribute("Production").Value),
+                    Gold = int.Parse(r.Attribute("Gold").Value)
+                }
+            );
+
+        return resourceData;
+    }
 }
