bfbdfd4c1e5c87883a8ce1dc8e920752b5293bb9
jeffercizer <jeffercize@gmail.com>
2025-03-27 11:58:50 -0400

    Update Yields.cs

--- Yields.cs
+++ Yields.cs
@@ -13,4 +13,19 @@ public struct Yields
     public float scienceYield;
     public float cultureYield;
     public float happinessYield;
+    
+    // Overload the + operator
+    public static Yields operator +(Yields a, Yields b)
+    {
+        return new Yields
+        {
+            foodYield = a.foodYield + b.foodYield,
+            productionYield = a.productionYield + b.productionYield,
+            productionOverflow = a.productionOverflow + b.productionOverflow,
+            goldYield = a.goldYield + b.goldYield,
+            scienceYield = a.scienceYield + b.scienceYield,
+            cultureYield = a.cultureYield + b.cultureYield,
+            happinessYield = a.happinessYield + b.happinessYield
+        };
+    }
 }
