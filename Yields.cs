69cc4b0e34fdb1e555b0e2f76172213721e8f1d8
jeffercizer <jeffercize@gmail.com>
2025-03-27 11:16:35 -0400

    Create Yields.cs

---
+++ Yields.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Data;
+
+public struct Yields
+{
+    public float foodYield;
+    public float productionYield;
+    public float productionOverflow;
+    public float goldYield;
+    public float scienceYield;
+    public float cultureYield;
+    public float happinessYield;
+}
