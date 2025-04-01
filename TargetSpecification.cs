51fadc223cc9f9a41977ff69646e5585a052160e
jeffercizer <jeffercize@gmail.com>
2025-04-01 10:48:28 -0400

    Create TargetSpecification.cs

---
+++ TargetSpecification.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Data;
+using System.Formats.Asn1;
+
+public class TargetSpecification
+{
+    public bool AllowsAnyUnit { get; set; } = false;
+    public bool AllowsAnyBuilding { get; set; } = false; // New flag for wildcard building targets
+    public HashSet<UnitType> ValidUnitTypes { get; set; } = new HashSet<UnitType>();
+    public UnitClass AllowedUnitClasses { get; set; } = UnitClass.None;
+    public HashSet<BuildingType> ValidBuildingTypes { get; set; } = new HashSet<BuildingType>();
+    public HashSet<TerrainType> ValidTerrainTypes { get; set; } = new HashSet<TerrainType>();
+
+    public bool IsValidTarget(UnitType? unitType, UnitClass? unitClass, BuildingType? buildingType, TerrainType? terrainType)
+    {
+        if (AllowsAnyUnit && unitType.HasValue)
+            return true;
+
+        if (AllowsAnyBuilding && buildingType.HasValue)
+            return true;
+
+        if (unitType.HasValue && !ValidUnitTypes.Contains(unitType.Value))
+            return false;
+
+        if (unitClass.HasValue && (AllowedUnitClasses & unitClass.Value) == 0)
+            return false;
+
+        if (buildingType.HasValue && !ValidBuildingTypes.Contains(buildingType.Value))
+            return false;
+
+        if (terrainType.HasValue && !ValidTerrainTypes.Contains(terrainType.Value))
+            return false;
+
+        return true;
+    }
+}
